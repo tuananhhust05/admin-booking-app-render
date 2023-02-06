d04ca2e7dcbfed4c863b30cfeae85f26942d0bf0
tuananhhust05 <tuananhhust05@gmail.com>
2023-02-06 19:12:33 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,10 +1,17 @@
 import { io } from "socket.io-client" 
-let socket = io("http://localhost:8800");
-
+let socket = io('https://api-booking-app-aws-ec2.onrender.com', {
+    secure: true,
+    enabledTransports: ["wss"],
+    transports: ['websocket', 'polling'],
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    maxReconnectionAttempts: 100
+});
 export const socketCient = ()=>{
     return socket;
 }
 
 export const url = ()=>{
-    return "http://localhost:8800/api"
+    return "https://api-booking-app-aws-ec2.onrender.com/api"
 }
